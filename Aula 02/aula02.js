a71d3dcdaacc3943f7e75151384d6349d9d4048d
carlosferreira123 <122993323+carlosferreira123@users.noreply.github.com>
2024-06-23 21:29:57 -0300

    Atualizaçao

---
+++ Aula 02/aula02.js
@@ -0,0 +1,14 @@
+
+/*Strings
+const numero = 23
+
+const texto = `o numero é ${numero}`
+
+console.log(texto)*/
+
+
+/*Numbers
+const numero = 200 + 20 
+console.log(numero)*/
+
+const variavel = false
