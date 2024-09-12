7f28dd6ed7cd9907af6b63b1e8d1fc28feeb4f5b
Carlos Alisson <carlosalisson01@gmail.com>
2024-09-12 20:56:54 -0300

    Atuliazação

---
+++ Aula 08/aula.js
@@ -0,0 +1,10 @@
+// grouping Operador (ordem de precedencia)
+
+let total = 2 + 3 * 4
+console.log(total)
+
+let total2 = (2+3) * 4
+console.log(total2)
+
+let average = 9.5 + 7 + 5/3
+console.log(average)
