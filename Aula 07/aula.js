c81e7ea455209eaea68fe135f1371a4bbdcaf6e9
Carlos Alisson <carlosalisson01@gmail.com>
2024-09-12 19:37:45 -0300

    Aulas Aritmeticos

---
+++ Aula 07/aula.js
@@ -0,0 +1,14 @@
+// O)peradores aritméticos
+
+console.log("Soma:  ", 12 + 8 )
+console.log("concxatenação:" , "12" + "8")
+
+console.log ("Subtração: " , 12 - 8)
+
+console.log ("MNultiplicação: ", 12 * 3)
+ 
+console.log("Divisao:", 12 / 2)
+
+console.log("Resto da divisão: ", 13 % 2)
+
+console.log("Exponencial: ", 3 ** 3)
