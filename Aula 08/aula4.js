3118271e28d3fe4018058677d08a7d31c506924f
Carlos Alisson <carlosalisson01@gmail.com>
2024-09-12 21:11:31 -0300

    Atulioazação

---
+++ Aula 08/aula4.js
@@ -0,0 +1,14 @@
+
+let balance = 500
+let payment = 120
+
+// > Maior que 
+console.log(balance > payment)
+
+// < Menor que
+console.log9balance < payment
+
+// Maior ou igual a >=
+balance = 120
+
+console.log(balance >= payment)
