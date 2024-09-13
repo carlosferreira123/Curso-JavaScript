d483e4f5bc192e98a9d3459c8eddbc4826390905
Carlos Alisson <carlosalisson01@gmail.com>
2024-09-12 21:23:08 -0300

    Atualização

---
+++ Aula 08/aula5.js
@@ -0,0 +1,31 @@
+let value
+
+// Operador atribuição
+
+value = 1
+console.log(value)
+
+//Incremento
+value += 2
+console.log(value)
+
+//Decremento
+value -= 1
+console.log(value)
+
+
+//Multiplicar
+value *= 3
+console.log(value)
+
+//Divisão 
+value /= 2
+console.log(value)
+
+//Resto da divisão
+value %= 2
+console.log(value)
+
+// Potencia
+value  **= 2
+console.log(value)
---
+++ Aula 08/aula6.js
@@ -0,0 +1,11 @@
+let email = true
+let password = true
+
+// AND (E) &&
+console.log(email && password)
+
+// or (ou) ||
+console.log(email || password )
+
+// NOT (negação) !
+console.log(!password)
