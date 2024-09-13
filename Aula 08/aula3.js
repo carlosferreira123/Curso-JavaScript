42584950e71fff8204e14fbf5483328cece18b9c
Carlos Alisson <carlosalisson01@gmail.com>
2024-09-12 21:07:26 -0300

    Atulização

---
+++ Aula 08/aula3.js
@@ -0,0 +1,11 @@
+let one = 1
+let two = 2
+
+// estritamente igual a (tipo e valor)
+
+console.log(one === 1)
+console.log(one === "1")
+
+// !== estritamento diferente de
+console.log(one !== two)
+console.log(one !== 1)
