96939db0fa27ae68e700eebb7f3842df4cfad94f
Carlos Alisson <carlosalisson01@gmail.com>
2024-09-13 20:32:53 -0300

    IF

---
+++ Aula 09/aula2.js
@@ -0,0 +1,14 @@
+// if (se)
+
+let hour = 11
+
+
+/* if (hour <= 12){
+   console.log("Bom dia!")
+   console.log("Seja bem vindo!")
+}
+   */
+
+if (hour <= 12)
+   console.log("Bom dia")
+   console.log("Seja bem vindo")
