5e7b95eded91f5f83791b3134e6f2a011ff1eea2
Carlos Alisson <carlosalisson01@gmail.com>
2024-09-28 20:09:49 -0300

    Adicionando conteudo

---
+++ Aula 11/scripts03.js
@@ -0,0 +1,8 @@
+const guest = document.querySelector("#guest-1 ")
+
+// Retorna o conteudo  como texto.
+// console.log(guest.textContent)
+// guest.textContent = "Maria"
+
+console.log(guest.textContent) // Retorna o conteudo visivel e oculto
+console.log(guest.innerText) // Retoena somente o conteudo visivel
