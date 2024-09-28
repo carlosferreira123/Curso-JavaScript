8bb4621d7364fe279c1d8ab396a1d66e3b414279
Carlos Alisson <carlosalisson01@gmail.com>
2024-09-28 20:16:27 -0300

    Adiconando

---
+++ Aula 11/scripts04.js
@@ -0,0 +1,9 @@
+const guest = document.querySelector("#guest-1 ")
+
+// Retorna o conteudo  como texto.
+// console.log(guest.textContent)
+// guest.textContent = "Maria"
+
+console.log(guest.textContent) // Retorna o conteudo visivel e oculto
+console.log(guest.innerText) // Retoena somente o conteudo visivel
+console.log(guest.innerHTML) // Retorna o HTML como texto
